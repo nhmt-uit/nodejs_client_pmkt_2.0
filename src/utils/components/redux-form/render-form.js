abd4eaa9977f8bb449a6e1164daea117e54cf965
khoanguyen2610 <anhkhoa.nguyen2610@gmail.com>
2019-07-22 18:29:36 +0700

    change validate form

--- src/utils/components/redux-form/render-form.js
+++ src/utils/components/redux-form/render-form.js
@@ -6,7 +6,7 @@ import { TransComponent } from 'my-components'
 export const renderError = props => {
     const {touched, error, warning } = props.meta
     return (
-        touched && error && <section><span {...props} className='error'><TransComponent i18nKey={error} /></span></section> || null
+        error && <section><span {...props} className='error'><TransComponent i18nKey={error} /></span></section> || null
     )
 }
 
